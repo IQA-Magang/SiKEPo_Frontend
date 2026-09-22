6bd3ada7484f8c112fcf0d7be8b403ca3ebaaf1b
akhrf <ahmadkarta9@gmail.com>
2026-09-22 11:37:21 +0700

    update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000';
+export const API_BASE = (import.meta.env.VITE_API_BASE || 'http://localhost:5000').replace(/\/+$/, '');
 
 const TOKEN_KEY = 'sikepo_token';
 const USER_KEY = 'sikepo_user';
